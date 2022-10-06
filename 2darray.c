33167f12711e7fc9030bd912f289783b502aa594
David P <davkojak@hotmail.com>
2022-10-06 00:38:22 -0400

    first commit

---
+++ 2darray.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+
+int	**intarray(int x, int y)
+{
+	int i;
+	int **a;
+    
+	a = (int **)malloc(x * sizeof(int*));
+	while (i < x)
+	{
+		a[i] = (int *)malloc(y * sizeof(int));
+        i++;
+    }
+    return (a);
+}
+
+void free_intarray(int **a, int x, int y)
+{
+    int i;
+    
+    i = 0;
+    while (i < x)
+    {
+        free(a[i]);
+        i++;
+    }
+}
+
+char **chararray(int x, int y)
+{
+    int i;
+    char **a;
+    a = (char **)malloc(x * sizeof(char*));
+    while (i < x)
+    {
+        a[i] = (char *)malloc(y * sizeof(char));
+        i++;
+    }
+    return (a); 
+}
+
+void free_chararray(char **a, int x, int y)
+{
+    int i;
+    
+    i = 0;
+    while (i < x)
+    {
+        free(a[i]);
+        i++;
+    }
+}
