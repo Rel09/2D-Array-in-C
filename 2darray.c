22e38f5e23540e8fa6b3d2f5d68eccece4462a5c
David P <89156264+Rel09@users.noreply.github.com>
2023-01-19 11:25:22 -0400

    Update 2darray.c
    update - 19/1/2023

--- 2darray.c
+++ 2darray.c
@@ -8,23 +8,19 @@ int	**intarray(int x, int y)
 	i = 0;
 	a = (int **)malloc(x * sizeof(int*));
 	while (i < x)
-	{
-		a[i] = (int *)malloc(y * sizeof(int));
-        i++;
+		a[i++] = (int *)malloc(y * sizeof(int));
     }
     return (a);
 }
 
-void free_intarray(int **a, int x, int y)
+void free_intarray(int **a, int x)
 {
     int i;
     
     i = 0;
     while (i < x)
-    {
-        free(a[i]);
-        i++;
-    }
+        free(a[i++]);
+    free(a);
 }
 
 char **chararray(int x, int y)
@@ -35,21 +31,16 @@ char **chararray(int x, int y)
     i = 0;
     a = (char **)malloc(x * sizeof(char*));
     while (i < x)
-    {
-        a[i] = (char *)malloc(y * sizeof(char));
-        i++;
-    }
+        a[i++] = (char *)malloc(y * sizeof(char));
     return (a); 
 }
 
-void free_chararray(char **a, int x, int y)
+void free_chararray(char **a, int x)
 {
     int i;
     
     i = 0;
     while (i < x)
-    {
-        free(a[i]);
-        i++;
-    }
+        free(a[i++]);
+    free(a);
 }
